327b49faef5efebc4e8c62a06555dd7a37a3c31f
pauloestrella <pauloestrella1994@gmail.com>
2021-10-05 14:10:32 -0300

    Add validation to competition over and fix dart-throw-competiton serializer

--- dart_throw/serializers.py
+++ dart_throw/serializers.py
@@ -12,7 +12,15 @@ class DartThrowAthletesSerializer(serializers.ModelSerializer):
             'unit_measurement',
         ]
 
+    def validate(self, data):
+        competition_end_date = data["competition"].end_date
+        if competition_end_date:
+            raise serializers.ValidationError("The competition is over")
+        return data
+
 class DartThrowCompetitionSerializer(serializers.ModelSerializer):
+    athletes_results = DartThrowAthletesSerializer(many=True, read_only=True, source='competition_id')
+    
     class Meta:
         model = DartThrowCompetition
         fields = [
