7ab7e3262d8b818306840f8757a3c6f3ff874b69
pauloestrella <pauloestrella1994@gmail.com>
2021-10-05 00:18:13 -0300

    Add views for dart throw

--- dart_throw/views.py
+++ dart_throw/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import DartThrowAthletes, DartThrowCompetition
+from .serializers import DartThrowAthletesSerializer, DartThrowCompetitionSerializer
+
+
+class DartThrowAthletesViewSet(viewsets.ModelViewSet):
+    queryset = DartThrowAthletes.objects.all()
+    serializer_class = DartThrowAthletesSerializer
+
+class DartThrowCompetitionViewSet(viewsets.ModelViewSet):
+    queryset = DartThrowCompetition.objects.all()
+    serializer_class = DartThrowCompetitionSerializer
