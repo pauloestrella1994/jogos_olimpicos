30f27ce3ec8642600f96167ebeca9ddda3dade18
pauloestrella <pauloestrella1994@gmail.com>
2021-10-04 23:03:31 -0300

    Add validator to competition serializer if have a competition and start and end a competition on the same request

--- swimming/serializers.py
+++ swimming/serializers.py
@@ -1,4 +1,4 @@
-from django.db.models import fields
+from django.db import transaction
 from rest_framework import serializers, validators
 from .models import SwimmingAthletes, SwimmingCompetition
 
@@ -40,9 +40,23 @@ class SwimmingCompetitionSerializer(serializers.ModelSerializer):
         ]
     
     def validate(self, data):
-        start_date = data.get("start_date", self.instance.start_date)
-        end_date = data.get("end_date", None)
-            
-        if start_date > end_date:
+        start_date = data.get("start_date")
+        end_date = data.get("end_date")
+        
+        if start_date is None:
+            try:
+                start_date = self.instance.start_date
+            except:
+                raise serializers.DjangoValidationError("Competition can't be created without a start date")
+
+        if end_date is not None and start_date > end_date:
             raise serializers.ValidationError("Start date competition can't be later then end date")
+        
+        if end_date:
+            with transaction.atomic():
+                try:
+                    self.instance.results = "competition ended"
+                    self.instance.save()
+                except:
+                    raise serializers.DjangoValidationError("It is not possible to close a competition if there is not yet one.")
         return data
