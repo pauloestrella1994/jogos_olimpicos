7e760edca29c35974ec4ad6148d07a274cc539fb
pauloestrella <pauloestrella1994@gmail.com>
2021-10-05 14:26:52 -0300

    Add validation for start and end date and a transaction atomic to results

--- dart_throw/serializers.py
+++ dart_throw/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 from .models import DartThrowAthletes, DartThrowCompetition
 
@@ -31,3 +32,25 @@ class DartThrowCompetitionSerializer(serializers.ModelSerializer):
             'results',
             'athletes_results',
         ]        
+
+    def validate(self, data):
+        start_date = data.get("start_date")
+        end_date = data.get("end_date")
+        
+        if start_date is None:
+            try:
+                start_date = self.instance.start_date
+            except:
+                raise serializers.DjangoValidationError("Competition can't be created without a start date.")
+
+        if end_date is not None and start_date > end_date:
+            raise serializers.ValidationError("Start date competition can't be later then end date.")
+        
+        if end_date:
+            with transaction.atomic():
+                try:
+                    self.instance.results = "competition ended"
+                    self.instance.save()
+                except:
+                    raise serializers.DjangoValidationError("It is not possible to close a competition if there is not yet one.")
+        return data
