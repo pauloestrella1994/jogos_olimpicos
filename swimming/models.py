6e0ab19849360ce269b95e23e8799adfa8248d95
pauloestrella <pauloestrella1994@gmail.com>
2021-10-05 00:16:28 -0300

    Remove default value for start_date competition in swimming

--- swimming/models.py
+++ swimming/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from datetime import datetime
 from django.db.models.deletion import CASCADE
 
 
 class SwimmingCompetition(models.Model):
     id = models.AutoField(primary_key=True, editable=False)
     competition_name = models.CharField(max_length=200, null=False, blank=False)
-    start_date = models.DateField(null=False, blank=False, default=str(datetime.now().date()))
+    start_date = models.DateField(null=False, blank=False)
     end_date = models.DateField(null=True, blank=True)
     results = models.CharField(max_length=20, null=False, blank=False, default="partial_result")
 
