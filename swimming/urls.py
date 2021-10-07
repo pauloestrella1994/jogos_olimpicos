0b7d08f8dbae18bcc7637732125684f53e2c4bb3
pauloestrella <pauloestrella1994@gmail.com>
2021-10-07 09:45:42 -0300

    Remove unusuable imports

--- swimming/urls.py
+++ swimming/urls.py
@@ -1,7 +1,5 @@
-from django import urls
 from django.urls import path
 from django.urls.conf import include
-from django.urls.resolvers import URLPattern
 from .views import SwimmingCompetitionViewSet, SwimmingAthletesViewSet
 from rest_framework import routers
 
